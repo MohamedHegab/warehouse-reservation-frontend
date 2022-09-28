9e1af4134f561a10f9d8ed1d09ac96af8c4cd420
Hegab <mohamed@factorial.co>
2022-09-28 20:25:22 +0200

    Working sockets

--- src/components/WarehouseListItem.tsx
+++ src/components/WarehouseListItem.tsx
@@ -1,6 +1,7 @@
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
+import { ActionCableConsumer } from 'react-actioncable-provider'
 
 type Props = {
   warehouse: any
@@ -9,11 +10,19 @@ type Props = {
 }
 const WarehouseListItem: React.FC<Props> = ({ warehouse, onClick, selectedWarehouseId }) => {
   // TODO: Add styling for the selected
+
+  const handleReceiveSlots = (response) => {
+    console.log({ response })
+  }
   return (
     <>
       <ListItem button alignItems="flex-start" onClick={onClick}>
         <ListItemText
           primary={warehouse?.name}
+        />
+        <ActionCableConsumer
+          channel={{ channel: 'ReservedSlotsChannel', id: 1 }}
+          onReceived={handleReceiveSlots}
         />
       </ListItem>
       <Divider component="li" />
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ActionCableProvider } from 'react-actioncable-provider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ActionCableProvider url={'ws://localhost:3000/cable'}>
+      <App />
+    </ActionCableProvider>
+  </React.StrictMode >
 )
